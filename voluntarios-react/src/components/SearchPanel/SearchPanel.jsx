082a56e40482f0ce675e978765750902f756df40
Hadassa Brenda <hadassabrenda90@gmail.com>
2026-06-14 20:48:51 -0300

    style: update component styles to use CSS variables for colors and improve consistency across the application

--- voluntarios-react/src/components/SearchPanel/SearchPanel.jsx
+++ voluntarios-react/src/components/SearchPanel/SearchPanel.jsx
@@ -1,9 +1,9 @@
 import { MapPin, SlidersHorizontal, Tag } from "lucide-react";
-
 import { CATEGORY_FILTER_OPTIONS } from "../../constants/categories";
 import { MODALITY_OPTIONS } from "../../constants/serviceOptions";
 import SelectField from "../SelectField/SelectField";
 import "./SearchPanel.css";
+import Button from "../Button/Button";
 
 export default function SearchPanel({ filters, onFilterChange }) {
   return (
@@ -31,20 +31,16 @@ export default function SearchPanel({ filters, onFilterChange }) {
         onChange={(value) => onFilterChange("modality", value)}
       />
 
-      <button className="search-panel__search-button" type="button">
-        <span style={{ fontWeight: "bold", color: "white", fontSize: "12px" }}>
-          Buscar
-        </span>
-      </button>
-
-      <button
+      <Button className="search-panel__search-button">
+        Buscar
+      </Button>
+     
+      <Button
         className="search-panel__advanced-button"
-        type="button"
-        style={{ fontSize: "12px" }}
+        icon={<SlidersHorizontal size={18} />}
       >
-        <SlidersHorizontal size={18} />
         Filtros avançados
-      </button>
+      </Button>
     </section>
   );
 }
